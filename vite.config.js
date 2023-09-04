b9f16d0df096cbbb8fe5371b8733551d44addf10
mzaini30 <muhzaini30@gmail.com>
2023-09-04 10:05:23 +0800

    upload dulu

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,20 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 import legacy from "@vitejs/plugin-legacy";
 import pages from "vite-plugin-pages-svelte";
 import uno_css from "unocss/vite";
-import transformerVariantGroup from '@unocss/transformer-variant-group'
+import transformerVariantGroup from "@unocss/transformer-variant-group";
+import extractor_svelte from "@unocss/extractor-svelte";
+import { presetUno } from "unocss";
 
-
-let plugins = [svelte(), pages(), uno_css({
-  // ...
-  transformers: [
-    transformerVariantGroup(),
-  ],
-})];
+let plugins = [
+  svelte(),
+  pages(),
+  uno_css({
+    // ...
+    transformers: [transformerVariantGroup()],
+    extractors: [extractor_svelte()],
+    presets: [presetUno()],
+  }),
+];
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
