d445fc08683151c5955d5cd0c3848b3f11e89fcc
mzaini30 <muhzaini30@gmail.com>
2023-09-04 09:33:12 +0800

    sudah selesai

---
+++ src/pages/index.svelte
@@ -0,0 +1,40 @@
+<div class="w-[80%] mx-auto sm:p-4">
+  <div class="">
+    <h1 class="<sm:font-bold sm:text-[50px] mt-10 mb-5">Support Zen 😊</h1>
+    <p>Yuk support Zen untuk membuat karya-karya bermanfaat lainnya... 💥</p>
+    <p class="italic <sm:mt-4">
+      Let's support Zen in creating more meaningful works... 🎶
+    </p>
+  </div>
+  <div
+    class="[&_.kotak]:(border rounded) my-10 [&_.judul]:(bg-gray-100) [&_.kotak>*]:p-3 grid grid-cols-1 sm:grid-cols-3 gap-4"
+  >
+    <div class="kotak">
+      <div class="judul">🏛️ Bank Kaltim</div>
+      <div class="isi">5000 0246 41</div>
+    </div>
+    <div class="kotak">
+      <div class="judul">📱 Gopay</div>
+      <div class="isi">0815 4514 3654</div>
+    </div>
+    <div class="kotak">
+      <div class="judul">🎫 Dana</div>
+      <div class="isi">0815 4514 3654</div>
+    </div>
+    <div class="kotak">
+      <div class="judul">🪙 Bitcoin</div>
+      <div class="isi">bc1qnw5gdrchs9e5wavhqhzqjh9ep4fppdngcphecg</div>
+    </div>
+
+    <div class="kotak">
+      <div class="judul">💦 Ethereum</div>
+      <div class="isi">0x61E0Bd4509B027501e92774d2aDDFc4478E06b58</div>
+    </div>
+    <div class="kotak">
+      <div class="judul">🌱 Algorand</div>
+      <div class="isi">
+        MLBC6QLMA7TIE6TAUUDQOMPUFBS35JTQH7U75I52BEF7V6FI3NW2ZGIZQI
+      </div>
+    </div>
+  </div>
+</div>
